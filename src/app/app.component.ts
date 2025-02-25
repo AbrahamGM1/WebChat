d8c20d7c27bd7809a4befd5228b187949a39a135
ABRAHAM <abraham.gomez228796@potros.itson.edu.mx>
2025-02-24 23:16:58 -0700

    boceto de la UI del chat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChatlistComponent } from './modules/chatlist/chatlist.component';
+import { ChatComponent } from './modules/chat/chat.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ChatlistComponent, ChatComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/modules/chatlist/chatlist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chatlist',
+  standalone: true,
+  imports: [],
+  templateUrl: './chatlist.component.html',
+  styleUrl: './chatlist.component.scss'
+})
+export class ChatlistComponent {
+
+}
