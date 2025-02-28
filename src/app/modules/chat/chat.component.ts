015d3bfa5cf058638b23507df7bcbe749597eb13
ABRAHAM <abraham.gomez228796@potros.itson.edu.mx>
2025-02-27 21:17:34 -0700

    Cambio en el HTML, ahora con el bind de Angular incorporado junto con datos dummies en typescript, mejorada la visualización de datos en la UI

--- src/app/modules/chat/chat.component.ts
+++ src/app/modules/chat/chat.component.ts
@@ -9,5 +9,47 @@ import { Component } from '@angular/core';
   styleUrl: './chat.component.scss'
 })
 export class ChatComponent {
+  date = new Date();
+  formattedDate:string = this.date.toLocaleString();
+
+  chat_users = [{
+    name:'Usuario 1',
+    message: 'Este es un mensaje de prueba del primer usuario',
+    date:this.formattedDate,
+    messages:[
+      {
+        id:'receiver',
+        message:'Este es un mensaje de prueba mandado por el receptor',
+        date:this.formattedDate,
+      },
+      {
+        id:'sender',
+        message:'Este es un mensaje de prueba mandado por el emisor',
+        date:this.formattedDate,
+      },
+      {
+        id:'sender',
+        message:'Este es un mensaje de prueba mandado por el emisor',
+        date:this.formattedDate,
+      },
+      {
+        id:'receiver',
+        message:'Este es un mensaje de prueba mandado por el receptor',
+        date:this.formattedDate,
+      }
+    ]
+  },{
+    name:'Usuario 2',
+    message: 'Este es un mensaje de prueba del segundo usuario',
+    date:this.formattedDate
+  },{
+    name:'Usuario 3',
+    message: 'Este es un mensaje de prueba del tercer usuario',
+    date:this.formattedDate
+  },{
+    name:'Usuario 4',
+    message: 'Este es un mensaje de prueba del cuarto usuario',
+    date:this.formattedDate
+  }]
 
 }
